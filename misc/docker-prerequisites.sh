90502e7b10f8eca870f5d684117dc41a19a81b8a
Vlad Zaharia <vlad@zaharia.io>
2020-07-30 20:27:12 -0700

    Update azcli

--- misc/docker-prerequisites.sh
+++ misc/docker-prerequisites.sh
@@ -31,4 +31,13 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Azure CLI
+curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+    
+sudo apt-get update
 sudo apt-get install azure-cli
